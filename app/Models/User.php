750b3d26042dacf7fd1dbc2af41feffe49650594
dewacahaya <dewacahaya30@gmail.com>
2026-03-09 14:17:32 +0800

    fix: avatar url when using google login

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -54,6 +57,26 @@ class User extends Authenticatable
             'last_study_at' => 'datetime',
             'energy_replenished_at' => 'datetime',
         ];
+    }
+
+    protected function avatarUrl(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value) {
+                // Kalau kosong, kembalikan null
+                if (!$value) {
+                    return null;
+                }
+
+                // Kalau URL-nya dari Google (sudah ada http/https), kembalikan apa adanya
+                if (Str::startsWith($value, ['http://', 'https://'])) {
+                    return $value;
+                }
+
+                // Kalau dari upload manual, tambahkan URL storage lokal
+                return asset('storage/' . $value);
+            },
+        );
     }
 
     public function progress()
